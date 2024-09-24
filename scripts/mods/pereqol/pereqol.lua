27876245341be86b5dc6882331e77f0e34217139
Hazard (Cyprian Skrzypczak) <jamajpro@gmail.com>
2024-09-24 19:59:14 +0200

    adding 3200 mastery to players

--- scripts/mods/pereqol/pereqol.lua
+++ scripts/mods/pereqol/pereqol.lua
@@ -194,6 +194,31 @@ mod.update = function(dt)
                         mod:dofile("scripts/mods/pereqol/blocking")
 
                         mod:rebal_changes()
+                        
+                        mod:hook(BackendInterfaceWeavesPlayFab, "get_mastery", function (func, self, career_name, optional_item_backend_id)
+                            local mastery_settings = WeaveMasterySettings
+                            local loadout = self._loadouts[career_name]
+                            local initial_mastery = 3200
+                            local cost = loadout and self:_get_loadout_mastery_cost(loadout) or 0
+                            if loadout then
+                                if loadout.slot_ranged then
+                                    local loadout_weapon_ranged = loadout.item_loadouts[loadout.slot_ranged]
+                                    if loadout_weapon_ranged then
+                                        cost = cost + self:_get_loadout_mastery_cost(loadout_weapon_ranged)
+                                    end
+                                end
+                                if loadout.slot_melee then
+                                    local loadout_weapon_melee = loadout.item_loadouts[loadout.slot_melee] 
+                                    if loadout_weapon_melee then
+                                        cost = cost + self:_get_loadout_mastery_cost(loadout_weapon_melee)
+                                    end
+                                end
+                            end
+                            local current_mastery = initial_mastery - cost    
+                            return initial_mastery, current_mastery
+                        end)
+
+
 					end
 				end
 			end
--- scripts/mods/pereqol/unlock_athanor.lua
+++ scripts/mods/pereqol/unlock_athanor.lua
@@ -3,26 +3,27 @@ local mod = get_mod("pereqol")
 mod:hook(HeroWindowWeaveProperties, "_magic_level", function(func,self) 
     return 30
 end)
-`
-
 
 mod:hook(BackendInterfaceWeavesPlayFab, "get_mastery", function (func, self, career_name, optional_item_backend_id)
 	local mastery_settings = WeaveMasterySettings
 	local loadout = self._loadouts[career_name]
-	local initial_mastery = 3000
-
-	if optional_item_backend_id then
-		loadout = loadout.item_loadouts[optional_item_backend_id]
-
-		local magic_level = self:get_item_magic_level(optional_item_backend_id)
-
-	else
-		local magic_level = self:get_career_magic_level(career_name)
-
-	end
-
-	local total_cost = loadout and self:_get_loadout_mastery_cost(loadout) or 0
-	local current_mastery = initial_mastery - total_cost
-
+	local initial_mastery = 3200
+    local cost = loadout and self:_get_loadout_mastery_cost(loadout) or 0
+    if loadout then
+        if loadout.slot_ranged then
+            local loadout_weapon_ranged = loadout.item_loadouts[loadout.slot_ranged]
+            if loadout_weapon_ranged then
+                cost = cost + self:_get_loadout_mastery_cost(loadout_weapon_ranged)
+            end
+        end
+        if loadout.slot_melee then
+            local loadout_weapon_melee = loadout.item_loadouts[loadout.slot_melee] 
+            if loadout_weapon_melee then
+                cost = cost + self:_get_loadout_mastery_cost(loadout_weapon_melee)
+            end
+        end
+    end
+	local current_mastery = initial_mastery - cost    
 	return initial_mastery, current_mastery
 end)
+
